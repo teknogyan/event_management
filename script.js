2d65aa82e28d56f3f50ede9908225ca80774d350
tekno <teknogyan123@gmail.com>
2024-03-08 18:08:57 +0530

    functioning mobile menu

--- script.js
+++ script.js
@@ -20,15 +20,30 @@
 //   },
 // });
 
-const swiper = new Swiper('.swiper', {
-  effect: 'slider',
+const swiper = new Swiper(".swiper", {
+  effect: "slider",
   navigation: {
-         nextEl: '.swiper-button-next',
-         prevEl: '.swiper-button-prev',
-     },
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 const menu_button = document.getElementById("hamburger-button");
 const close_button = document.getElementById("close-button");
+const menu = document.getElementById("mobile-menu");
+const logo = document.getElementById("logo");
 
-menu_button.addEventListener()
+menu_button.addEventListener('click', () =>{
+logo.style.display = "none"
+menu.style.display= "flex"
+menu_button.style.display = "none";
+close_button.style.display = "block"
+});
+
+close_button.addEventListener('click', () => {
+  logo.style.display = "block"
+menu.style.display= "none"
+menu_button.style.display = "block";
+close_button.style.display = "none"
+})
+
